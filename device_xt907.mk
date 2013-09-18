5828a099e07cb1e003e3e085f8fb547a8a5277d3
Tim Pohlad-Thomas <timpohladthomas@gmail.com>
2013-09-18 16:17:56 -0400

    change to qcom-common

--- device_xt907.mk
+++ device_xt907.mk
@@ -16,7 +16,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-$(call inherit-product, device/motorola/msm8960-common/msm8960.mk)
+$(call inherit-product, device/motorola/qcom-common/qcom-common.mk)
 
 LOCAL_PATH := device/motorola/xt907
 
